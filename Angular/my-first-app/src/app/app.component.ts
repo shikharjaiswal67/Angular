3d580809f0546e709721a45ff3553fabc4623eaf
shikharjaiswal <shikharjaiswal67@gmail.com>
2019-07-10 13:08:08 +1000

    updates with multi components and modules, styles, and template

--- Angular/my-first-app/src/app/app.component.ts
+++ Angular/my-first-app/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
+  // Below `` is used for multi line statements
+  styles: [`
+  h3{
+   color: deepskyblue;
+  }
+  `]
 })
 export class AppComponent {
   title = 'my-first-app';
