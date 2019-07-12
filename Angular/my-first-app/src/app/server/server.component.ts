0e0c31361918f402ba4c2ae9bc2322692e62db73
shikharjaiswal <shikharjaiswal67@gmail.com>
2019-07-12 18:17:01 +1000

    updates with multi components and modules, styles, and template

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -1,9 +1,31 @@
 import {Component} from '@angular/core';
 
+
 @Component({
 
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'Online';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'Green' : 'Red';
+  }
+}
+
---
+++ Angular/my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>Warning, you are in danger !!</p>
+  `
+})
+
+export class WarningAlertComponent {}
